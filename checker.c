e87453e10d344d273b5d6ede5f73e3ec49bbd02e
a-hakki <abdelazizhatake@gmail.com>
2025-01-30 16:13:41 +0100

    checker done, mndatory and bonus as well

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:42:48 by ahakki            #+#    #+#             */
-/*   Updated: 2025/01/30 13:29:48 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/01/30 14:24:49 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	push_swap_language(char *buffer, t_list **a, t_list **b)
 	if (ft_strncmp(buffer, "ss\n", 3) == 0)
 		return (swap(b, a), 1);
 	if (ft_strncmp(buffer, "ra\n", 3) == 0)
-		return (ra_rb(b, 's'), 1);
+		return (ra_rb(a, 's'), 1);
 	if (ft_strncmp(buffer, "rb\n", 3) == 0)
 		return (ra_rb(b, 's'), 1);
 	if (ft_strncmp(buffer, "rr\n", 3) == 0)
@@ -37,7 +37,7 @@ int	push_swap_language(char *buffer, t_list **a, t_list **b)
 	if (ft_strncmp(buffer, "pb\n", 3) == 0)
 		return (pb_pa(a, b, 's'), 1);
 	get_next_line(-1);
-	return (ftc(*a), ftc(*b), printfd(1, "Error\n"), free(buffer), exit(1), 0);
+	return (ftc(*a), ftc(*b), printfd(2, "Error\n"), free(buffer), exit(1), 0);
 }
 
 int	ft_checker(t_list **a, t_list **b, int i)
@@ -49,10 +49,10 @@ int	ft_checker(t_list **a, t_list **b, int i)
 	while (temp && temp->next)
 	{
 		if (temp->content > temp->next->content)
-			return ((ftc(*a), ftc(*b), write(1, "\033[31mKO\n\033[0m", 8), exit(1), 0));
+			return ((ftc(*a), ftc(*b), write(1, "KO\n", 3), exit(1), 0));
 		temp = temp->next;
 	}
-	return (ftc(*a), ftc(*b), write(1, "\033[32mOK\n\033[0m", 8), exit(0), 0);
+	return (ftc(*a), ftc(*b), write(1, "OK\n", 3), exit(0), 0);
 }
 
 int	main(int ac, char **av)
